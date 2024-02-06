8be6de7f89510c64180803e988747b1dd19ed931
FosterSetor9060 <walkerdziwornu@gmail.com>
2024-02-06 13:45:13 +0000

    python io setor_dzi model

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+7-add_item.py
+#!/usr/bin/python3
+"""Defininf a class Student."""
+
+
+class Student:
+    """Representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Getting a dictionary representation of the Student."""
+        return self.__dict__
