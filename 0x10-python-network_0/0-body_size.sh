f9614b394f3ecc7424f73fd15f6edd959e671229
Foster Setor <walkerdziwornu@gmail.com>
2024-04-25 20:45:42 -0700

    curl operation_pn

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# This script takes a URL as input and sending request to the URL
-
-# Send a GET request to the provided URL
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
-
+# that takes in a URL and displays the size of the body of the response
+curl -sI $1 | grep -i Content-Length |awk '{print $2}'
