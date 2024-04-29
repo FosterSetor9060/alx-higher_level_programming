88e2307605492c1a6cde9d7c42aae5c043596f4e
Foster Setor <walkerdziwornu@gmail.com>
2024-04-29 01:28:44 -0700

    update on python_networking-ws HTTP requests roles

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request and displays
+displays the body of the response (decoded in utf-8).variable 
+found in the header
+(handling HTTP errors)
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
